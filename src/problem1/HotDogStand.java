c9184eb324c2b14cecfde07412dc34a41216b40e
BlastoiseBlue <emmet@stanevich.com>
2017-10-31 12:56:23 -0400

    Created HotDogStand class

--- src/problem1/HotDogStand.java
+++ src/problem1/HotDogStand.java
@@ -6,22 +6,42 @@
 package problem1;
 
 public class HotDogStand {
-	private int id;
-	private int numSold;
+	private int id = 0;
+	private int numSold = 0;
+	static int total = 0;
 
 	public void justSold() {
 		numSold++;
+		total++;
+	}
+
+	public void justSold(int sold) {
+		if (sold > 0) {
+			numSold = numSold + sold;
+			total = total + sold;
+		}
 	}
 
 	public int getNumSold() {
 		return numSold;
 	}
 
+	public static int getTotal() {
+		return total;
+	}
+
+	public HotDogStand() {
+		id = 0;
+		numSold = 0;
+	}
+
+	public HotDogStand(int inputId) {
+		id = inputId;
+	}
+
 	public HotDogStand(int inputId, int inputSold) {
 		id = inputId;
 		numSold = inputSold;
+		total = total + inputSold;
 	}
-	// public static int totalSold() {
-	//
-	// }
 }
